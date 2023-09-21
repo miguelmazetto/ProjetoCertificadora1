d641e6e47ffb1b71fddf790a291a79af026f00ea
miguelmazetto <33629994+miguelmazetto@users.noreply.github.com>
2023-09-21 20:05:35 -0300

    Contextualizar o exemplo no nosso problema

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,28 +14,14 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<strong class="text-xl uppercase">
+					Projeto de Certificadora 1
+				</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<a
 					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
+					href="https://github.com/miguelmazetto/ProjetoCertificadora1"
 					target="_blank"
 					rel="noreferrer"
 				>
