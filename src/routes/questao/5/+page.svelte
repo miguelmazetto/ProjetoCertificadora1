4c54afe9a39cfef618d6a943e6549818f45c2494
AndrSilvajr <zzzandree@gmail.com>
2023-10-11 18:05:13 -0300

    Questoes 1 - 10

---
+++ src/routes/questao/5/+page.svelte
@@ -0,0 +1,12 @@
+<div class="card p-4 m-4">
+	<p class="text-center text-2xl">
+    	<b>Questão 5</b>
+	</p>
+	<p>Qual é a Terceira Lei de Newton, também conhecida como a Lei da Ação e Reação?</p>
+	<select class="select">
+		<option value="1">"Um objeto em movimento tende a permanecer em movimento, e um objeto em repouso tende a permanecer em repouso."</option>
+		<option value="2">"A força resultante sobre um objeto é igual à massa do objeto multiplicada pela aceleração."</option>
+		<option value="3">"Para cada ação, há uma reação igual e oposta."</option>
+		<option value="4"> "A energia não pode ser criada nem destruída, apenas transformada."</option>
+	</select>
+</div>
---
+++ src/routes/questao/9/+page.svelte
@@ -0,0 +1,12 @@
+<div class="card p-4 m-4">
+	<p class="text-center text-2xl">
+    	<b>Questão 9</b>
+	</p>
+	<p>Qual é a unidade SI da carga elétrica?</p>
+	<select class="select">
+		<option value="1">Coulombs (C)</option>
+		<option value="2">Ohms (Ω)</option>
+		<option value="3">Amperes (A)</option>
+		<option value="4">Volts (V)</option>
+	</select>
+</div>
