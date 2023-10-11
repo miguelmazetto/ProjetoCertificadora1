da4c8a857866f6ae03726a8a2f64ee6981e7ddde
AndrSilvajr <zzzandree@gmail.com>
2023-10-11 17:14:08 -0300

    requep

--- src/routes/questao/1/+page.svelte
+++ src/routes/questao/1/+page.svelte
@@ -2,12 +2,5 @@
 	<p class="text-center text-2xl">
     	<b>Questão 1</b>
 	</p>
-	<p>Qual é a unidade de medida da velocidade no Sistema Internacional (SI)?</p>
-	<select class="select">
-		<option value="1">Metros por segundo (m/s)</option>
-		<option value="2">Newtons (N)</option>
-		<option value="3">Joules (J)</option>
-		<option value="4">Watts (W)</option>
-	</select>
-	
+	<p>dasdasdasdsDSA</p>
 </div>
