ffbf12fa9171eb85fcc312ef6c87aadae38fc800
AndrSilvajr <zzzandree@gmail.com>
2023-10-11 16:31:12 -0300

    ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,3 +5,9 @@
 <p><a href="/questao/3">Questao 3</a></p>
 <p><a href="/questao/4">Questao 4</a></p>
 <p><a href="/questao/5">Questao 5</a></p>
+<p><a href="/questao/6">Questao 6</a></p>
+<p><a href="/questao/7">Questao 7</a></p>
+<p><a href="/questao/8">Questao 8</a></p>
+<p><a href="/questao/9">Questao 9</a></p>
+<p><a href="/questao/10">Questao 10</a></p>
+
