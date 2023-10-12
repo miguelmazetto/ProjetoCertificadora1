bee6bf20a667bcd1c2d0be1e1ff59629ae160620
AndrSilvajr <zzzandree@gmail.com>
2023-10-12 19:14:28 -0300

    Dificuldade

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,17 +3,32 @@
 
 
 <center>
+    <p class="text-center text-2xl">
+    	<b>Fácil</b>
+	</p>
     <div class="btn-group-vertical variant-ghost" >
         <p><a href="/questao/1">Questao 1</a></p>
         <p><a href="/questao/2">Questao 2</a></p>
         <p><a href="/questao/3">Questao 3</a></p>
         <p><a href="/questao/4">Questao 4</a></p>
+        
+        
+    </div> 
+    <p class="text-center text-2xl">
+    	<b>Média</b>
+	</p>
+    <div  class="btn-group-vertical variant-ghost">
         <p><a href="/questao/5">Questao 5</a></p>
         <p><a href="/questao/6">Questao 6</a></p>
         <p><a href="/questao/7">Questao 7</a></p>
+    </div>
+    <p class="text-center text-2xl">
+    	<b>Difícil</b>
+	</p>
+    <div  class="btn-group-vertical variant-ghost">
         <p><a href="/questao/8">Questao 8</a></p>
         <p><a href="/questao/9">Questao 9</a></p>
         <p><a href="/questao/10">Questao 10</a></p>
-    </div>  
+    </div>
 </center>
 
